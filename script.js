3cd5a03a581900c5bc5c6c36fcb5e49c694396d6
Arjun Biju <arjunbiju332@gmail.com>
2023-09-19 21:37:47 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+    
+    $(document).ready(function(){
+       
+    
+        $("#web").mouseenter(function(){
+            $(this).css({"background":"blue"})
+        })
+    
+         $("#web").mouseleave(function(){
+            $(this).css({"background":""})
+        })
+    
+        
+    })
+    
+    
